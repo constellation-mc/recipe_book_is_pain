9e6e0f2540ad4164b90cc8cd057f790ef7e048b7
melontini <melontini@yandex.ru>
2022-07-12 19:37:47 +0700

    @Shadow of the year

--- src/main/java/me/melontini/recipebookispain/mixin/RecipeBookWidgetMixin.java
+++ src/main/java/me/melontini/recipebookispain/mixin/RecipeBookWidgetMixin.java
@@ -50,9 +50,6 @@ public abstract class RecipeBookWidgetMixin {
     @Unique
     private StateSwitchingButton prevPageButton;
 
-    @Shadow
-    private boolean visible;
-
     @Shadow public abstract boolean isVisible();
 
     @Inject(at = @At("RETURN"), method = "init")
