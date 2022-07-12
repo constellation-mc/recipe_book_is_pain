200152cdb980d8818be561b6b20ed0e4241f2304
melontini <104443436+melontini@users.noreply.github.com>
2022-07-13 00:37:42 +0700

    Revert "MojMap"
    This reverts commit 02ed48c47803112aedf901096370ef474c129ede.

--- src/main/java/me/melontini/recipebookispain/mixin/Accessor.java
+++ src/main/java/me/melontini/recipebookispain/mixin/Accessor.java
@@ -1,14 +1,14 @@
 package me.melontini.recipebookispain.mixin;
 
-import net.minecraft.client.RecipeBookCategories;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.client.recipebook.RecipeBookGroup;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(RecipeBookCategories.class)
+@Mixin(RecipeBookGroup.class)
 public interface Accessor {
     @Invoker("<init>")
-    static RecipeBookCategories newGroup(String internalName, int internalId, ItemStack... stacks) {
+    static RecipeBookGroup newGroup(String internalName, int internalId, ItemStack... stacks) {
         throw new AssertionError();
     }
 }
