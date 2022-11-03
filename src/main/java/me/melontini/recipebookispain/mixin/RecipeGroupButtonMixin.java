b539c1e1e3248c0fa41bac4462be720c523ea92d
melontini <104443436+melontini@users.noreply.github.com>
2022-11-03 14:54:28 +0700

    forge forced me to rewrite this mod thrice!!! yaaaay!!!

--- src/main/java/me/melontini/recipebookispain/mixin/RecipeGroupButtonMixin.java
+++ src/main/java/me/melontini/recipebookispain/mixin/RecipeGroupButtonMixin.java
@@ -1,17 +1,9 @@
 package me.melontini.recipebookispain.mixin;
 
-import me.melontini.recipebookispain.RecipeBookIsPain;
 import me.melontini.recipebookispain.access.RecipeGroupButtonAccess;
 import net.minecraft.client.gui.screen.recipebook.RecipeGroupButtonWidget;
-import net.minecraft.client.recipebook.RecipeBookGroup;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Collections;
-import java.util.List;
 
 @Mixin(RecipeGroupButtonWidget.class)
 public class RecipeGroupButtonMixin implements RecipeGroupButtonAccess {
@@ -26,12 +18,5 @@ public class RecipeGroupButtonMixin implements RecipeGroupButtonAccess {
     @Override
     public void setPage(int page) {
         this.page = page;
-    }
-
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/recipebook/RecipeBookGroup;getIcons()Ljava/util/List;"), method = "renderIcons")
-    private List<ItemStack> render(RecipeBookGroup instance) {
-        if (RecipeBookIsPain.ADDED_GROUPS.containsValue(instance))
-            return Collections.singletonList(RecipeBookIsPain.AAAAAAAA.get(instance.name()).getIcon());
-        return instance.getIcons();
     }
 }
