0f705911379cfa3e7e1aa6b58af962fe5f7a4193
melontini <104443436+melontini@users.noreply.github.com>
2023-04-25 22:58:56 +0700

    ...

--- src/main/java/me/melontini/recipebookispain/RecipeBookIsPainClient.java
+++ src/main/java/me/melontini/recipebookispain/RecipeBookIsPainClient.java
@@ -1,12 +1,12 @@
 package me.melontini.recipebookispain;
 
+import me.melontini.crackerutil.util.PrependingLogger;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.item.ItemGroup;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,12 +14,11 @@ import java.util.Map;
 @Environment(EnvType.CLIENT)
 public class RecipeBookIsPainClient implements ClientModInitializer {
 
-    public static final Logger LOGGER = LogManager.getLogger("RBIP");
+    public static final PrependingLogger LOGGER = new PrependingLogger(LogManager.getLogger("RBIP"), PrependingLogger.LOGGER_NAME);
     public static Map<RecipeBookGroup, ItemGroup> RECIPE_BOOK_GROUP_TO_ITEM_GROUP = new HashMap<>();
     public static Map<ItemGroup, RecipeBookGroup> ITEM_GROUP_TO_RECIPE_BOOK_GROUP = new HashMap<>();
 
     @Override
     public void onInitializeClient() {
-        //null pointers are epic
     }
 }
