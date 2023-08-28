8fe689f8eb1533111abdc110a2186b9001ca5484
melontini <104443436+melontini@users.noreply.github.com>
2023-08-28 15:51:43 +0700

    Switched to Optional in some places.

--- src/main/java/me/melontini/recipebookispain/mixin/groups/ClientRecipeBookMixin.java
+++ src/main/java/me/melontini/recipebookispain/mixin/groups/ClientRecipeBookMixin.java
@@ -18,9 +18,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
-import static me.melontini.recipebookispain.RecipeBookIsPainClient.CRAFTING_LIST;
-import static me.melontini.recipebookispain.RecipeBookIsPainClient.CRAFTING_SEARCH_LIST;
+import static me.melontini.recipebookispain.RecipeBookIsPainClient.*;
 
 @Mixin(value = ClientRecipeBook.class, priority = 999)
 public class ClientRecipeBookMixin {
@@ -40,13 +40,12 @@ public class ClientRecipeBookMixin {
                     try {
                         RecipeBookGroup recipeBookGroup = RecipeBookHelper.createGroup(new Identifier("rbip", "crafting_" + itemGroup.getIndex()), itemGroup.getIcon());
                         RecipeBookIsPainClient.RECIPE_BOOK_GROUP_TO_ITEM_GROUP.put(recipeBookGroup, itemGroup);
-                        RecipeBookIsPainClient.ITEM_GROUP_TO_RECIPE_BOOK_GROUP.put(itemGroup, recipeBookGroup);
+                        ITEM_GROUP_TO_RECIPE_BOOK_GROUP.put(itemGroup, recipeBookGroup);
 
                         CRAFTING_LIST.add(recipeBookGroup);
                         CRAFTING_SEARCH_LIST.add(recipeBookGroup);
                     } catch (Exception e) {
-                        String info = String.format("%s item group", itemGroup.getName());
-                        RecipeBookIsPainClient.LOGGER.error(String.format("Error while processing %s", info), e);
+                        RecipeBookIsPainClient.LOGGER.error("Error while processing %s item group".formatted(itemGroup.getName()), e);
                     }
                 });
         CRAFTING_LIST.add(0, RecipeBookGroup.CRAFTING_SEARCH);
@@ -60,16 +59,10 @@ public class ClientRecipeBookMixin {
     private static void rbip$getGroupForRecipe(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookGroup> cir) {
         if (RecipeType.CRAFTING.equals(recipe.getType())) {
             ItemStack itemStack = recipe.getOutput();
-            ItemGroup group = itemStack.getItem().getGroup();
-            if (group != null) {
-                if (group != ItemGroup.HOTBAR && group != ItemGroup.INVENTORY && group != ItemGroup.SEARCH) {
-                    if (RecipeBookIsPainClient.ITEM_GROUP_TO_RECIPE_BOOK_GROUP.containsKey(group)) {
-                        cir.setReturnValue(RecipeBookIsPainClient.ITEM_GROUP_TO_RECIPE_BOOK_GROUP.get(group));
-                        return;
-                    }
-                }
-            }
-            cir.setReturnValue(RecipeBookIsPainClient.ITEM_GROUP_TO_RECIPE_BOOK_GROUP.get(ItemGroup.MISC));
+            Optional.ofNullable(itemStack.getItem().getGroup())
+                    .filter(group -> !group.isSpecial() && group != ItemGroup.INVENTORY && group != ItemGroup.SEARCH)
+                    .map(ITEM_GROUP_TO_RECIPE_BOOK_GROUP::get)
+                    .ifPresentOrElse(cir::setReturnValue, () -> cir.setReturnValue(ITEM_GROUP_TO_RECIPE_BOOK_GROUP.get(ItemGroup.MISC)));
         }
     }
 
